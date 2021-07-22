39c46c0f919131a5eca735d737aea7b54cf7d978
Programmer <niraj.itnare@apisero.com>
2021-07-22 17:39:23 +0530

    master is comitting

---
+++ src/masterClassPackage/Main.java
@@ -0,0 +1,11 @@
+package masterClassPackage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello from master personal");
+		
+	}
+
+}
